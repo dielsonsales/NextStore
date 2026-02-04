1750c3abbd8769cdd1560270d3aaa1a953b6626e
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-02-04 16:52:32 -0300

    feat: Create profile routes

---
+++ src/app/(profile)/edit-profile/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function EditProfilePage() {
+  return <h1>Editar Perfil</h1>;
+}
---
+++ src/app/(profile)/profile/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function ProfilePage() {
+  return <h1>Perfil</h1>;
+}
