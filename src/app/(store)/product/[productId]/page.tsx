32f31a318dbaa481d5a701fc5c8f6ce12616b316
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-02-04 16:37:08 -0300

    feat: Create main routes

---
+++ src/app/(store)/product/[productId]/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function ProductDetailPage() {
+  return <h1>Product</h1>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main>
-      <h1>NextStore</h1>
-    </main>
-  );
+  redirect("/products");
 }
