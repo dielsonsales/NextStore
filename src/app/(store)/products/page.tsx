6f7ff59f6967f7786d5adec5bc5b829ccc39532b
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-02-05 15:45:00 -0300

    feat: Implement grid layout for the products listing

--- src/app/(store)/products/page.tsx
+++ src/app/(store)/products/page.tsx
@@ -3,17 +3,24 @@ import styles from "./page.module.css";
 
 export default function ProductsPage() {
   const MOCK_PRODUCTS = [
-    { id: 1, title: "Product A", description: "Um produto muito bom" },
-    { id: 2, title: "Product B", description: "Outro producto muito bom" },
-    { id: 3, title: "Product C", description: "Esse já não é tão bom" },
+    { id: 1, title: "Product 1", description: "Um produto muito bom" },
+    { id: 2, title: "Product 2", description: "Outro producto muito bom" },
+    { id: 3, title: "Product 3", description: "Esse já não é tão bom" },
+    { id: 4, title: "Product 4", description: "Esse já não é tão bom" },
+    { id: 5, title: "Product 5", description: "Esse já não é tão bom" },
+    { id: 6, title: "Product 6", description: "Esse já não é tão bom" },
+    { id: 7, title: "Product 7", description: "Esse já não é tão bom" },
+    { id: 8, title: "Product 8", description: "Esse já não é tão bom" },
+    { id: 9, title: "Product 9", description: "Esse já não é tão bom" },
+    { id: 10, title: "Product 10", description: "Esse já não é tão bom" },
   ];
 
   return (
     <>
       <h1>Produtos</h1>
-      <ul>
+      <ul className={styles.productsGrid}>
         {MOCK_PRODUCTS.map((product) => (
-          <li key={product.id}>
+          <li key={product.id} className={styles.productCard}>
             <article>
               <h3>{product.title}</h3>
               <p>{product.description}</p>
