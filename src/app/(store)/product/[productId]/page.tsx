55077d95a282fcafbdf7b2f0e1048bd3ac9487f1
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-02-04 22:44:56 -0300

    feat: Implement product detail with ISR

--- src/app/(store)/product/[productId]/page.tsx
+++ src/app/(store)/product/[productId]/page.tsx
@@ -1,5 +1,15 @@
 import styles from "./page.module.css";
 
+export const dynamicParams = true; // allows dynamic routes too
+export const revalidate = 3600; // 1 hour
+
+export async function generateStaticParams() {
+  const productIds = ["1", "2", "3"];
+  return productIds.map((id) => ({
+    productId: id,
+  }));
+}
+
 interface ProductDetailPageProps {
   params: Promise<{ productId: string }>;
 }
@@ -8,5 +18,10 @@ export default async function ProductDetailPage({
   params,
 }: ProductDetailPageProps) {
   const { productId } = await params;
-  return <h1>Product {productId}</h1>;
+  return (
+    <>
+      <h1>Product {productId}</h1>
+      <p>This page was generated/update via ISR.</p>
+    </>
+  );
 }
--- src/app/(store)/products/page.tsx
+++ src/app/(store)/products/page.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function ProductsPage() {
-  return <h1>Produtos</h1>;
+  const MOCK_PRODUCTS = [
+    { id: 1, title: "Product A", description: "Um produto muito bom" },
+    { id: 2, title: "Product B", description: "Outro producto muito bom" },
+    { id: 3, title: "Product C", description: "Esse já não é tão bom" },
+  ];
+  return (
+    <>
+      <h1>Produtos</h1>
+      <ul>
+        {MOCK_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <Link href={`/product/${product.id}`}>Ver detalhes</Link>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
