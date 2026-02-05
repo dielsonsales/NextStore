bb32a25791bdf957592adfef8ebb27d27195b27f
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-02-05 01:29:10 -0300

    feat: Create edit user page with personalized content

--- src/app/(profile)/edit-profile/page.tsx
+++ src/app/(profile)/edit-profile/page.tsx
@@ -1,5 +1,52 @@
+"use client";
+
+import { useEffect, useEffectEvent, useState } from "react";
 import styles from "./page.module.css";
 
+const MOCK_USER_KEY = "mock_user";
+
 export default function EditProfilePage() {
-  return <h1>Editar Perfil</h1>;
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    const savedData = localStorage.getItem(MOCK_USER_KEY);
+    if (savedData) {
+      setUser(JSON.parse(savedData));
+    } else {
+      setUser({ name: "Usuário 1", email: "usuario1@next-store.com.br" });
+    }
+  }, []);
+
+  const handleSave = useEffectEvent(() => {
+    localStorage.setItem(MOCK_USER_KEY, JSON.stringify(user));
+  });
+
+  return (
+    <>
+      <h1>Editar Perfil</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSave();
+        }}
+      >
+        <input
+          value={user.name}
+          onChange={(e) => {
+            setUser({ ...user, name: e.target.value });
+          }}
+        />
+        <input
+          value={user.email}
+          onChange={(e) => {
+            setUser({ ...user, email: e.target.value });
+          }}
+        />
+        <button type="submit">Salvar Alterações</button>
+      </form>
+    </>
+  );
 }
