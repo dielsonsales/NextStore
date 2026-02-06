a56a787ca57c8ba15e7114384d1762a81b6ac28f
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-02-05 22:22:38 -0300

    feat: Add images to products

--- src/app/(store)/products/page.tsx
+++ src/app/(store)/products/page.tsx
@@ -1,27 +1,65 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import { Product } from "@/lib/types";
+import Image from "next/image";
 
 export default function ProductsPage() {
-  const MOCK_PRODUCTS = [
-    { id: 1, title: "Product 1", description: "Um produto muito bom" },
-    { id: 2, title: "Product 2", description: "Outro producto muito bom" },
-    { id: 3, title: "Product 3", description: "Esse já não é tão bom" },
-    { id: 4, title: "Product 4", description: "Esse já não é tão bom" },
-    { id: 5, title: "Product 5", description: "Esse já não é tão bom" },
-    { id: 6, title: "Product 6", description: "Esse já não é tão bom" },
-    { id: 7, title: "Product 7", description: "Esse já não é tão bom" },
-    { id: 8, title: "Product 8", description: "Esse já não é tão bom" },
-    { id: 9, title: "Product 9", description: "Esse já não é tão bom" },
-    { id: 10, title: "Product 10", description: "Esse já não é tão bom" },
+  const MOCK_PRODUCTS: Product[] = [
+    {
+      id: 1,
+      title: "Camisa feminina",
+      description:
+        "linda camisa florida, mod.croped, mangas longas, elástico no punho",
+      image: "2e694b2fa2aa5e020ab5f461665aac6a.jpg",
+    },
+    {
+      id: 2,
+      title: "Blusa feminina",
+      description:
+        "blusa feminina sem manga, confeccionada em tecido leve e fluido",
+      image: "7b70718c8fad11a5a03b3e2a292f3d7c.jpg",
+    },
+    {
+      id: 3,
+      title: "louis vuitton boulogne mm - original - vintage - monograma",
+      description: "modelo vintage - original. Made in France.",
+      image: "69cffd174c76d5f046c725a58f3563a7.jpg",
+    },
+    {
+      id: 4,
+      title: "Short jeans",
+      description: "tamanho 42, peça conservada",
+      image: "89f5fe35d4023e8c5642076d02d12b05.jpg",
+    },
+    {
+      id: 5,
+      title: "Tênis adidas feminino gazelle collegiate green",
+      description: "muito moda na europa",
+      image: "d55c59d06303bc42881fc45a8e204947.jpg",
+    },
+    {
+      id: 6,
+      title: "Vestido amarelo mob",
+      description:
+        "detalhe de conservação: estado bom 8/10 (médio uso, tecido desfiado na foto)",
+      image: "f38346e74a6a29d5f16837611c91e6b6.jpg",
+    },
   ];
 
   return (
     <>
-      <h1>Produtos</h1>
+      <h1>Next Store</h1>
       <ul className={styles.productsGrid}>
         {MOCK_PRODUCTS.map((product) => (
           <li key={product.id} className={styles.productCard}>
             <article>
+              <Image
+                className={styles.productImage}
+                src={`/images/${product.image}`}
+                alt={product.title}
+                width={500}
+                height={500}
+              />
               <h3>{product.title}</h3>
               <p>{product.description}</p>
               <Link href={`/product/${product.id}`}>Ver detalhes</Link>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,3 +2,10 @@ export interface User {
   name: string;
   email: string;
 }
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+}
