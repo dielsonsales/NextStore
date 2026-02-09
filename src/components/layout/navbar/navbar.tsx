1dc21a45d14d031913e57743fd54a2eae3298558
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-02-09 01:39:47 -0300

    fix: Remove unnecessary <span> inside <Link>

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -4,11 +4,11 @@ import styles from "./navbar.module.css";
 export default function NavBar() {
   return (
     <nav className={styles.navBarContainer}>
-      <Link href="/products">
-        <span className={styles.navBarText}>Store</span>
+      <Link className={styles.navBarText} href="/products">
+        Store
       </Link>
-      <Link href="/profile">
-        <span className={styles.navBarText}>Profile</span>
+      <Link className={styles.navBarText} href="/profile">
+        Profile
       </Link>
     </nav>
   );
