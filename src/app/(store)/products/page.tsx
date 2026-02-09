85f1432ab08d5657d22a6cf0e55c839750bd30d1
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-02-09 01:52:27 -0300

    feat: Create a section for the products area for accessibility

--- src/app/(store)/products/page.tsx
+++ src/app/(store)/products/page.tsx
@@ -7,29 +7,31 @@ export default function ProductsPage() {
   return (
     <>
       <h1 className="pageTitle1">Next Store</h1>
-      <ul className={styles.productsGrid}>
-        {MOCK_PRODUCTS.map((product) => (
-          <li key={product.id} className={styles.productCard}>
-            <article style={{ display: "contents" }}>
-              <Image
-                className={styles.productImage}
-                src={`/images/${product.image}`}
-                alt={product.title}
-                width={500}
-                height={500}
-              />
-              <h2 className={styles.productTitle}>{product.title}</h2>
-              <p className={styles.productText}>{product.description}</p>
-              <Link
-                className={styles.productLink}
-                href={`/product/${product.id}`}
-              >
-                <span className={styles.productText}>Ver detalhes</span>
-              </Link>
-            </article>
-          </li>
-        ))}
-      </ul>
+      <section aria-label="Products">
+        <ul className={styles.productsGrid}>
+          {MOCK_PRODUCTS.map((product) => (
+            <li key={product.id} className={styles.productCard}>
+              <article style={{ display: "contents" }}>
+                <Image
+                  className={styles.productImage}
+                  src={`/images/${product.image}`}
+                  alt={product.title}
+                  width={500}
+                  height={500}
+                />
+                <h2 className={styles.productTitle}>{product.title}</h2>
+                <p className={styles.productText}>{product.description}</p>
+                <Link
+                  className={styles.productLink}
+                  href={`/product/${product.id}`}
+                >
+                  <span className={styles.productText}>Ver detalhes</span>
+                </Link>
+              </article>
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 }
