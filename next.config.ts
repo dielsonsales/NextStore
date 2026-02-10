964f29b5d135aad9c0145c9cf4c65c20289437e7
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-02-09 21:45:14 -0300

    chore: Disables the persistent cache of turbopack

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   experimental: {
-    turbopackFileSystemCacheForDev: true, // for faster restarts
+    turbopackFileSystemCacheForDev: false, // see https://www.reddit.com/r/nextjs/comments/1qzuahr/psa_if_you_use_nextjs_161_turborepo_exclude/
   },
 };
 
